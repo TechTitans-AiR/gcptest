eed9ab3d6b28ceedda43c1adf3ee9e1c76f2c64b
tsopic30082000 <tsopic@foi.hr>
2023-11-23 23:57:04 +0100

    Added JSON serializer for ObjectIDs

--- src/main/java/hr/techtitans/users/models/UserRole.java
+++ src/main/java/hr/techtitans/users/models/UserRole.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @Document(collection="users_role")
 @Data
@@ -14,6 +16,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 public class UserRole {
     @Id
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     private String name;
     private String code;
